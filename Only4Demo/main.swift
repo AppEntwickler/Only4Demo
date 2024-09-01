26a50dbbc54c6e9f680390ffb28f7f70a6870e52
KRAⓂ️ikER AppDesigning <AppDesigning@Outlook.de>
2024-09-01 17:28:16 +0200

    Von wegen "Geht Nicht" "Geht Nicht, Gibt's Nicht!"

--- Only4Demo/main.swift
+++ Only4Demo/main.swift
@@ -9,12 +9,9 @@ import Foundation
 
 print("Only4Demo auf MacOS erstellt")
 
-
 print("Only4Demo auf MacOS mit Xcode Push auf GitHub")
 
-
 print("Only4Demo auf Windows mit VisualStudio Clone von GitHub")
-
 
 print("Only4Demo auf Windows mit VisualStudio modifiziert und mit Push auf GitHub")
 
